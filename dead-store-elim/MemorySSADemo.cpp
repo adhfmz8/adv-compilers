13555a65794a53874f8b2ddc58fbb21c7b9f8bb3
adhfmz8 <73725651+adhfmz8@users.noreply.github.com>
2025-11-12 12:44:19 -0700

    part1 memssa

--- dead-store-elim/MemorySSADemo.cpp
+++ dead-store-elim/MemorySSADemo.cpp
@@ -12,43 +12,68 @@ struct MemorySSADemoPass : PassInfoMixin<MemorySSADemoPass> {
     auto& MSSAResult = AM.getResult<MemorySSAAnalysis>(F);
     auto& MSSA = MSSAResult.getMSSA();
 
-    errs() << "Analyzing function: " << F.getName() << "\n";
+    errs() << "digraph \"MemorySSA_" << F.getName() << "\" {\n";
+    errs() << "  node [shape=box, style=filled, fillcolor=lightgray];\n";
 
-    // Iterate over basic blocks to show all MemoryAccesses
+    DenseMap<const MemoryAccess*, std::string> NodeNames;
+    unsigned ID = 0;
+
+    // Assign unique names
     for (auto& BB : F) {
-      errs() << "BasicBlock: " << BB.getName() << "\n";
-
-      // MemoryPhi nodes are found at block entries
-      if (auto* Phi = MSSA.getMemoryAccess(&BB)) {
-        if (auto* MPhi = dyn_cast<MemoryPhi>(Phi)) {
-          errs() << "  MemoryPhi for block " << BB.getName() << ":\n";
-          for (unsigned i = 0; i < MPhi->getNumIncomingValues(); ++i) {
-            auto* IncomingAcc = MPhi->getIncomingValue(i);
-            auto* Pred = MPhi->getIncomingBlock(i);
-            errs() << "    from " << Pred->getName() << ": ";
-            IncomingAcc->print(errs());
-            errs() << "\n";
-          }
-        }
-      }
-
-      // Iterate over instructions for MemoryDef/Use
       for (auto& I : BB) {
-        if (auto* MA = MSSA.getMemoryAccess(&I)) {
-          errs() << "  ";
-          MA->print(errs());
-          errs() << "\n";
+        if (auto* MA = MSSA.getMemoryAccess(&I))
+          NodeNames[MA] = "n" + std::to_string(ID++);
+      }
+      if (auto* Phi = MSSA.getMemoryAccess(&BB))
+        NodeNames[Phi] = "n" + std::to_string(ID++);
+    }
+
+    // Emit nodes
+    for (auto& Pair : NodeNames) {
+      const MemoryAccess* MA = Pair.first;
+      std::string Label;
+      raw_string_ostream LS(Label);
+      MA->print(LS);
+      LS.flush();
+
+      std::string SafeLabel;
+      for (char C : Label) SafeLabel += (C == '"') ? '\'' : C;
+
+      errs() << "  " << Pair.second << " [label=\"" << SafeLabel << "\"];\n";
+    }
+
+    // Emit edges
+    for (auto& Pair : NodeNames) {
+      const MemoryAccess* MA = Pair.first;
+
+      if (auto* Def = dyn_cast<MemoryDef>(MA)) {
+        if (auto* Src = Def->getDefiningAccess())
+          if (NodeNames.count(Src))
+            errs() << "  " << NodeNames[Src] << " -> " << NodeNames[MA]
+                   << ";\n";
+      } else if (auto* Use = dyn_cast<MemoryUse>(MA)) {
+        if (auto* Src = Use->getDefiningAccess())
+          if (NodeNames.count(Src))
+            errs() << "  " << NodeNames[Src] << " -> " << NodeNames[MA]
+                   << ";\n";
+      } else if (auto* Phi = dyn_cast<MemoryPhi>(MA)) {
+        for (unsigned i = 0; i < Phi->getNumIncomingValues(); ++i) {
+          auto* Incoming = Phi->getIncomingValue(i);
+          if (NodeNames.count(Incoming))
+            errs() << "  " << NodeNames[Incoming] << " -> " << NodeNames[MA]
+                   << ";\n";
         }
       }
     }
 
+    errs() << "}\n";
     return PreservedAnalyses::all();
   }
 };
 
 extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
 llvmGetPassPluginInfo() {
-  return {LLVM_PLUGIN_API_VERSION, "MemorySSADemoPass", "v0.9",
+  return {LLVM_PLUGIN_API_VERSION, "MemorySSADemoPass", "v1.1",
           [](PassBuilder& PB) {
             PB.registerAnalysisRegistrationCallback(
                 [](FunctionAnalysisManager& FAM) {
